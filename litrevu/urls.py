f705df909cf22e02b506ef02fba8ab8e7e3a691f
Ali FATHALLAH <alifathallah@yahoo.fr>
2023-08-26 00:53:31 +0200

    restart

--- litrevu/urls.py
+++ litrevu/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for LITREVU project.
+URL configuration for litrevu project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
