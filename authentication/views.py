9cdadaae328bd72cce8644579b4274609d58388e
Ali FATHALLAH <alifathallah@yahoo.fr>
2023-08-27 18:24:09 +0200

    authentication: Register, Login and Logout Done..

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.views.generic import View
+from django.conf import settings
+
+from . import forms
 
 # Create your views here.
+
+# class LoginPageView(View):
+#     template_name = 'authentication/login.html'
+#     form_class = forms.LoginForm
+
+#     def get(self, request):
+#         form = self.form_class()
+#         message = ''
+#         return render(request, self.template_name, context={'form': form, 'message': message})
+        
+#     def post(self, request):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             user = authenticate(
+#                 username=form.cleaned_data['username'],
+#                 password=form.cleaned_data['password'],
+#             )
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('home')
+#         message = 'Identifiants invalides.'
+#         return render(request, self.template_name, context={'form': form, 'message': message})
+
+class SignupPageView(View):
+    template_name = 'authentication/register.html'
+    form_calss = forms.SignupForm
+
+    def get(self, request):
+        form = self.form_calss()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})    
+
+    def post(self, request):
+        form = self.form_calss(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message}) 
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
--- litrevu/urls.py
+++ litrevu/urls.py
@@ -16,7 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
+
+import authentication.views
+import feed.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', authentication.views.LoginPageView.as_view(), name='login'),
+    path('', LoginView.as_view(
+            template_name='authentication/login.html',
+            redirect_authenticated_user=True),
+        name='login'),
+    path('logout/', authentication.views.logout_user, name='logout'),
+    path('home/', feed.views.feed, name='home'),
+    path('signup/', authentication.views.SignupPageView.as_view(), name='signup')
 ]
